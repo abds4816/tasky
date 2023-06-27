f78419becbb51644d147cc975c5f93481cee6acc
abds <abds4816@gmail.com>
2023-06-27 04:21:43 +0000

    add shadcn ui and theme toggle dropdown to navbar and update root layout and main page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ToggleTheme } from "./ToggleTheme";
+
+const Navbar = () => {
+  return (
+    <header>
+      <ToggleTheme />
+    </header>
+  );
+};
+
+export default Navbar;
