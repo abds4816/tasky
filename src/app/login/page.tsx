99002cde7c1928af44cd3f43fcb49d5638742a68
abds <abds4816@gmail.com>
2023-07-07 05:00:56 +0000

    create and test auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,6 +11,7 @@ export default function login() {
           Welcome Back to tasky
         </h2>
         <SignInButton />
+        <p>test</p>
         <p className="text-muted-foreground">
           By continuing, you are setting up a Breadit account and agree to our
           User Agreement and Privacy Policy.
