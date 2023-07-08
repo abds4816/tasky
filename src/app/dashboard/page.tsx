4eb00ea19a13dc177128461c55f703aa32f35e56
abds <abds4816@gmail.com>
2023-07-08 09:34:08 +0000

    test middleware

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,3 @@
-import { getAuthSession } from "@/lib/auth";
-import { redirect } from "next/navigation";
-import React from "react";
-
 export default async function dashboard() {
-  const session = await getAuthSession();
-
-  if (!session?.user) {
-    redirect("/login");
-  }
-
   return <div>dashboard</div>;
 }
