87a44871674ff19fc0cefc50b3fa1308fac3f900
abds <abds4816@gmail.com>
2023-07-12 01:10:03 +0000

    add header to settings page

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,5 +1,13 @@
+import PageHeader from "@/components/PageHeader";
 import React from "react";
 
 export default function settings() {
-  return <div>settings</div>;
+  return (
+    <div className="flex flex-col gap-y-6 md:gap-y-8">
+      <PageHeader
+        title="settings"
+        description="Here you can manage your account easely!"
+      />
+    </div>
+  );
 }
