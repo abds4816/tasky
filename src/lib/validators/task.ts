8cad5155d2675abad8a185b95aa90421586d8bfe
abds <abds4816@gmail.com>
2023-07-17 01:14:26 +0000

    add pricing cards and try to fix task creation

--- src/lib/validators/task.ts
+++ src/lib/validators/task.ts
@@ -4,10 +4,14 @@ export const TaskValidator = z.object({
   title: z
     .string({ required_error: "task title is required" })
     .min(3, "task title must be at least 3 characters"),
-  status: z.string({
+  status: z.enum(["TODO", "IN_PROGRESS", "DONE", "CANCELED"], {
     required_error: "Please select a status",
+    invalid_type_error: "Select a status",
   }),
-  priority: z.string({ required_error: "Please select a priority" }),
+  priority: z.enum(["LOW", "MEDIUM", "HIGH"], {
+    required_error: "Please select a priority",
+    invalid_type_error: "Select a priority",
+  }),
 });
 
 export type TaskRequest = z.infer<typeof TaskValidator>;
